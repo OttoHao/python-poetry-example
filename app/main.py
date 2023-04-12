ca8f9eef53ecc678f48bafb6c9a4aa67aeda2585
Minghao Pan <mpan2@slb.com>
2023-04-12 16:00:13 +0100

    init

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from models.people import People
+
+
+def main() -> None:
+    people = People(Name='Minghao')
+    print('hello world')
+    print(people)
+    while True:
+        pass
+
+
+if __name__ == "__main__":
+    main()
